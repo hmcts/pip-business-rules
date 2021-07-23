f25b63dfc2cb6a04ac481daa09b5216ef35cf21c
Kurt Plaschke <k.plaschke@hull.ac.uk>
2021-07-23 14:34:21 +0100

    changed url link

--- src/main/java/uk/gov/hmcts/reform/pip/rules/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/rules/config/SwaggerConfiguration.java
@@ -32,7 +32,7 @@ public class SwaggerConfiguration {
             .title("P&I Business Rules Service")
             .description("Use this service to view rules applied to the withholding of information not yet ready for " +
                              "release. You can find out more about how the code works at"
-                             + " https://github.com/hmcts/pip-subscription-management")
+                             + " https://github.com/hmcts/pip-business-rules")
             .version("1.0.0")
             .contact(new Contact("P&I Team",
                                  "https://tools.hmcts.net/confluence/display/PUBH/Publication+Hub",
