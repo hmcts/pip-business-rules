dfd10eaf32e9abd4f033c474e6b1d676bc6fda67
Christopher Pateman <christopher.pateman@version1.com>
2021-09-21 12:14:17 +0100

    Add smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
@@ -6,4 +6,11 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan("uk.gov.hmcts.reform.pip.rules.smoketests")
 @PropertySource("application.properties")
 public class SmokeTestConfiguration {
+
+    @Test
+    @DisplayName("Smoke Test")
+    public void test() {
+        assertEquals(1, 1,
+                     "Tested");
+    }
 }
