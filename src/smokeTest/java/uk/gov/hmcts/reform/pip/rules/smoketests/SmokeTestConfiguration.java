511abb07d0c68900fb25663831ee3362f73832eb
Christopher Pateman <christopher.pateman@version1.com>
2021-09-21 12:19:31 +0100

    Add Smoke Test

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
@@ -7,9 +7,27 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("application.properties")
 public class SmokeTestConfiguration {
 
+     @Value("${test.instance.uri}")
+    private String url;
+
+    private RestAssuredConfig config;
+
+    @Before
+    public void setUp() {
+        RestAssured.useRelaxedHTTPSValidation();
+        config = RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig()
+                        .setParam("http.connection.timeout", 60000)
+                        .setParam("http.socket.timeout", 60000)
+                        .setParam("http.connection-manager.timeout", 60000));
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1,
-                     "Tested");
+    public void shouldGetOkStatusFromHealthEndpoint() {
+        given().config(config)
+                .when()
+                .get(url + "/health")
+                .then()
+                .statusCode(HttpStatus.OK.value());
     }
 }
