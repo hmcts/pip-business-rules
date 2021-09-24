e06e45470459a855fd81a9461229c21c4235a591
Christopher Pateman <christopher.pateman@version1.com>
2021-09-24 14:51:14 +0100

    remove smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.pip.rules.smoketests;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-
-@ComponentScan("uk.gov.hmcts.reform.pip.rules.smoketests")
-@PropertySource("application.properties")
-public class SmokeTestConfiguration {
-}
